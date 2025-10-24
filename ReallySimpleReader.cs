270c6d07731467975321b84f677c412b24046039
Peter Rogers <p.rogers@gold.ac.uk>
2025-10-24 13:59:40 +0100

    Update ReallySimpleReader.cs

--- ReallySimpleReader.cs
+++ ReallySimpleReader.cs
@@ -5,6 +5,8 @@ public class ReallySimpleReader : MonoBehaviour
     public ArduinoCommunicator arduinoCommunicator; // drag it in if not found automatically
     public int idToRead = 0;                        // which Arduino value ID to show
     public float currentValue;                      // visible in Inspector
+    public float speed = 2f;
+    
 
     void Start()
     {
@@ -28,6 +30,12 @@ public class ReallySimpleReader : MonoBehaviour
         if (value != null)
         {
             currentValue = value.Value.GetValue();
+            transform.position = new Vector3(currentValue/100.0f, transform.position.y, transform.position.z);
+
+            //        
+            // Vector3 current = transform.position;
+            // float newX = Mathf.MoveTowards(current.x, currentValue, speed * Time.deltaTime);
+            // transform.position = new Vector3(newX, current.y, current.z);
         }
     }
 
